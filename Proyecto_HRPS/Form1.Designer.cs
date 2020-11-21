0ee864b90d64f2bfbaf0f5348f760d23674d87f2
JOSE UREÑA <jurena60946@ufide.ac.cr>
2020-11-20 22:47:19 -0600

    Se cambio y ahora dice BUENAS02

--- Proyecto_HRPS/Form1.Designer.cs
+++ Proyecto_HRPS/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(338, 194);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(66, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "BUENAS";
+            this.label1.Text = "BUENAS 02";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
