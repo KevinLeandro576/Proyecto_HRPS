7dc03da85b6253200e9b0a2881ce14d28c2c2883
Kevin Leandro <leandrokevin576@gmail.com>
2020-12-02 21:07:51 -0600

    Mas

--- Proyecto_HRPS/MenuDeEmpleado.Designer.cs
+++ Proyecto_HRPS/MenuDeEmpleado.Designer.cs
@@ -31,6 +31,7 @@
             this.enlace_Perfiles = new System.Windows.Forms.LinkLabel();
             this.enlace_HorasExtras = new System.Windows.Forms.LinkLabel();
             this.enlace_Solicitudes = new System.Windows.Forms.LinkLabel();
+            this.enlaceDeVolver = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // enlace_Perfiles
@@ -38,7 +39,7 @@
             this.enlace_Perfiles.AutoSize = true;
             this.enlace_Perfiles.Font = new System.Drawing.Font("Arial", 8.25F);
             this.enlace_Perfiles.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
-            this.enlace_Perfiles.Location = new System.Drawing.Point(103, 52);
+            this.enlace_Perfiles.Location = new System.Drawing.Point(12, 37);
             this.enlace_Perfiles.Name = "enlace_Perfiles";
             this.enlace_Perfiles.Size = new System.Drawing.Size(40, 14);
             this.enlace_Perfiles.TabIndex = 0;
@@ -50,7 +51,7 @@
             this.enlace_HorasExtras.AutoSize = true;
             this.enlace_HorasExtras.Font = new System.Drawing.Font("Arial", 8.25F);
             this.enlace_HorasExtras.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
-            this.enlace_HorasExtras.Location = new System.Drawing.Point(310, 52);
+            this.enlace_HorasExtras.Location = new System.Drawing.Point(151, 37);
             this.enlace_HorasExtras.Name = "enlace_HorasExtras";
             this.enlace_HorasExtras.Size = new System.Drawing.Size(112, 14);
             this.enlace_HorasExtras.TabIndex = 1;
@@ -62,12 +63,24 @@
             this.enlace_Solicitudes.AutoSize = true;
             this.enlace_Solicitudes.Font = new System.Drawing.Font("Arial", 8.25F);
             this.enlace_Solicitudes.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
-            this.enlace_Solicitudes.Location = new System.Drawing.Point(558, 52);
+            this.enlace_Solicitudes.Location = new System.Drawing.Point(445, 37);
             this.enlace_Solicitudes.Name = "enlace_Solicitudes";
             this.enlace_Solicitudes.Size = new System.Drawing.Size(72, 14);
             this.enlace_Solicitudes.TabIndex = 2;
             this.enlace_Solicitudes.TabStop = true;
             this.enlace_Solicitudes.Text = "SOLICITUDES";
+            // 
+            // enlaceDeVolver
+            // 
+            this.enlaceDeVolver.AutoSize = true;
+            this.enlaceDeVolver.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enlaceDeVolver.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
+            this.enlaceDeVolver.Location = new System.Drawing.Point(678, 37);
+            this.enlaceDeVolver.Name = "enlaceDeVolver";
+            this.enlaceDeVolver.Size = new System.Drawing.Size(49, 14);
+            this.enlaceDeVolver.TabIndex = 9;
+            this.enlaceDeVolver.TabStop = true;
+            this.enlaceDeVolver.Text = "VOLVER";
             // 
             // MenuDeEmpleado
             // 
@@ -75,6 +88,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.enlaceDeVolver);
             this.Controls.Add(this.enlace_Solicitudes);
             this.Controls.Add(this.enlace_HorasExtras);
             this.Controls.Add(this.enlace_Perfiles);
@@ -90,5 +104,6 @@
         private System.Windows.Forms.LinkLabel enlace_Perfiles;
         private System.Windows.Forms.LinkLabel enlace_HorasExtras;
         private System.Windows.Forms.LinkLabel enlace_Solicitudes;
+        private System.Windows.Forms.LinkLabel enlaceDeVolver;
     }
 }
