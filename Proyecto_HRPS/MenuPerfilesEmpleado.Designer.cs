a10f5684ecefebd37540ec7c0ae8a182ad07b9c3
Kevin Leandro <leandrokevin576@gmail.com>
2020-12-02 21:22:45 -0600

    Último_cambio

--- Proyecto_HRPS/MenuPerfilesEmpleado.Designer.cs
+++ Proyecto_HRPS/MenuPerfilesEmpleado.Designer.cs
@@ -31,7 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.botonDeVolver = new System.Windows.Forms.Button();
+            this.enlaceDeVolver = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // label1
@@ -39,7 +39,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 8.25F);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
-            this.label1.Location = new System.Drawing.Point(571, 72);
+            this.label1.Location = new System.Drawing.Point(464, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 14);
             this.label1.TabIndex = 0;
@@ -50,7 +50,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 8.25F);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
-            this.label2.Location = new System.Drawing.Point(356, 72);
+            this.label2.Location = new System.Drawing.Point(207, 42);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 14);
             this.label2.TabIndex = 1;
@@ -61,30 +61,30 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 8.25F);
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
-            this.label3.Location = new System.Drawing.Point(147, 72);
+            this.label3.Location = new System.Drawing.Point(12, 42);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(78, 14);
             this.label3.TabIndex = 2;
             this.label3.Text = "VER HORARIO";
             // 
-            // botonDeVolver
+            // enlaceDeVolver
             // 
-            this.botonDeVolver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
-            this.botonDeVolver.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.botonDeVolver.ForeColor = System.Drawing.Color.White;
-            this.botonDeVolver.Location = new System.Drawing.Point(99, 269);
-            this.botonDeVolver.Name = "botonDeVolver";
-            this.botonDeVolver.Size = new System.Drawing.Size(176, 48);
-            this.botonDeVolver.TabIndex = 16;
-            this.botonDeVolver.Text = "VOLVER";
-            this.botonDeVolver.UseVisualStyleBackColor = false;
+            this.enlaceDeVolver.AutoSize = true;
+            this.enlaceDeVolver.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enlaceDeVolver.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
+            this.enlaceDeVolver.Location = new System.Drawing.Point(657, 42);
+            this.enlaceDeVolver.Name = "enlaceDeVolver";
+            this.enlaceDeVolver.Size = new System.Drawing.Size(49, 14);
+            this.enlaceDeVolver.TabIndex = 10;
+            this.enlaceDeVolver.TabStop = true;
+            this.enlaceDeVolver.Text = "VOLVER";
             // 
             // MenuPerfilesEmpleado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.botonDeVolver);
+            this.Controls.Add(this.enlaceDeVolver);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -100,6 +100,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button botonDeVolver;
+        private System.Windows.Forms.LinkLabel enlaceDeVolver;
     }
 }
