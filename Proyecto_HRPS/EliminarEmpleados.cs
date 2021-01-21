ec8128cf3a20c98280ed46900a10615efb8b7258
JOSE UREÑA <jurena60946@ufide.ac.cr>
2021-01-21 17:59:22 -0600

    Ver texto en lo que sería botones de que se quitara empleado

--- Proyecto_HRPS/EliminarEmpleados.cs
+++ Proyecto_HRPS/EliminarEmpleados.cs
@@ -30,12 +30,21 @@ namespace Proyecto_HRPS
             //Hace botones de manera dinamica, hacer que se hagan por cada empleado registrado
             int startposition = 100;
             int endposition = 10;
-            for (int i = 0; i <= 10; i++)
+            var conexion = AbrirBaseDeDatos();
+            var comando = conexion.GetStoredProcCommand("ADMINISTRADOR_VER_EMPLEADOS_A_BORRAR");
+
+            using (IDataReader informacionEncontrada = conexion.ExecuteReader(comando))
             {
-                Button boton = agregarBoton(i, startposition, endposition);
-                panelDeFlujoDeEmpleados.Controls.Add(boton);
-                boton.Click += new System.EventHandler(this.clickAboton);
-                endposition += 100;
+                while (informacionEncontrada.Read())
+                {
+                    int i = 0;
+                    string cedula = informacionEncontrada["PK_CEDULA"].ToString();
+                    string nombre = informacionEncontrada["NOMBRE"].ToString();
+                    Button boton = agregarBoton(i, startposition, endposition, cedula, nombre);
+                    panelDeFlujoDeEmpleados.Controls.Add(boton);
+                    boton.Click += new System.EventHandler(this.clickAboton);
+                    endposition += 100;
+                }
             }
         }
         private void clickAboton(object sender, EventArgs e)
@@ -44,43 +53,29 @@ namespace Proyecto_HRPS
             MessageBox.Show("Hola");
             //Borrar empleado (ponerlo inactivo)
         }
-        Button agregarBoton(int i, int start, int end)
+        Button agregarBoton(int i, int start, int end, string cedula, string nombre)
         {
-            //Hace un boton, poner texto de empleado segun historia de usuario
+            //Hace un boton, poner texto de empleado segun historia de usuario    
             Button botonConEmpleado = new Button();
             botonConEmpleado.Name = "Empleado " + i.ToString();
-            botonConEmpleado.Text = "";
-            botonConEmpleado.ForeColor = Color.White;
+            botonConEmpleado.Text = "" + cedula + "\n" + nombre;
+            botonConEmpleado.ForeColor = Color.Black;
             botonConEmpleado.BackColor = Color.White;
-            botonConEmpleado.Font = new Font("Arial", 16, FontStyle.Regular);
-            botonConEmpleado.Width = 170;
-            botonConEmpleado.Height = 80;
+            botonConEmpleado.Font = new Font("Arial", 10, FontStyle.Regular);
+            botonConEmpleado.Width = 200;
+            botonConEmpleado.Height = 60;
             botonConEmpleado.Location = new Point(start, end);
             botonConEmpleado.TextAlign = ContentAlignment.MiddleCenter;
             botonConEmpleado.Margin = new Padding(5);
             return botonConEmpleado;
+
         }
+
         //POR CADA FORMULARIO CON CRUD
         public Database AbrirBaseDeDatos()
         {
             var connectionString = @"Server=tcp:servidor-de-hr-payroll-system.database.windows.net,1433;Initial Catalog=HR_PAYROLL_SYSTEM;Persist Security Info=False;User ID=Kevin;Password=Leandro123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             return new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase(connectionString);
         }
-        //POR CADA INSERT   
-        /*private void botonDeRegistrarUsuario_Click(object sender, EventArgs e)
-        {
-            var conexion = AbrirBaseDeDatos();
-            var comando = conexion.GetStoredProcCommand("ADMINISTRADOR_INSERTAR_EMPLEADO", textBoxDeCedula.Text,
-                                                                                            textBoxDeNombre.Text,
-                                                                                            textBoxDeHorario.Text,
-                                                                                            dateTimePickerDeFechaDeNacimiento.Value,
-                                                                                            int.Parse(textBoxDeSalario.Text),
-                                                                                            textBoxDePuesto.Text,
-                                                                                            decimal.Parse(textBoxDeSalarioPorHora.Text),
-                                                                                            textBoxDeTiempo.Text,
-                                                                                            textBoxDeContrasena.Text,
-                                                                                            int.Parse(textBoxDeIdDeRol.Text));
-            conexion.ExecuteNonQuery(comando);
-        }*/
     }
 }
