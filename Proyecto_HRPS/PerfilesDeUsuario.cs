97bdedc0d203883d4479779d521a6569f23cc569
JOSE UREÑA <jurena60946@ufide.ac.cr>
2021-01-22 03:25:15 -0600

    Sacar informacion para reporte

--- Proyecto_HRPS/PerfilesDeUsuario.cs
+++ Proyecto_HRPS/PerfilesDeUsuario.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Grid;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using Microsoft.Practices.EnterpriseLibrary.Data;
 
 namespace Proyecto_HRPS
 {
@@ -45,9 +52,30 @@ namespace Proyecto_HRPS
 
         private void enlaceDeVerEmpleados_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            VerEmpleados verEmpleados = new VerEmpleados();
+            var conexion = AbrirBaseDeDatos();
+            var comando = conexion.GetStoredProcCommand("ADMINISTRADOR_VER_EMPLEADOS_ACTIVOS");
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/pdfTechnician.pdf";
+            Document DC = new Document(PageSize.A4, 25, 25, 30, 30);
+            FileStream FS = File.Create(path);
+            PdfWriter.GetInstance(DC, FS);
+            DC.Open();
+            using (IDataReader informacionEncontrada = conexion.ExecuteReader(comando))
+            {
+                while (informacionEncontrada.Read())
+                {
+                    string cedula = informacionEncontrada["PK_CEDULA"].ToString();
+                    string nombre = informacionEncontrada["NOMBRE"].ToString();
+                    string puesto = informacionEncontrada["PUESTO"].ToString();
+
+                    string todo = cedula + " " + nombre + " " + puesto;
+
+                    DC.Add(new Paragraph(todo));
+                }
+            }
+            DC.Close();
+            /*VerEmpleados verEmpleados = new VerEmpleados();
             this.Hide();
-            verEmpleados.Show();
+            verEmpleados.Show();*/
         }
 
         private void enlaceDeModificarHorario_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -69,6 +97,11 @@ namespace Proyecto_HRPS
             EliminarEmpleados eliminarEmpleados = new EliminarEmpleados();
             this.Hide();
             eliminarEmpleados.Show();
+        }
+        public Database AbrirBaseDeDatos()
+        {
+            var connectionString = @"Server=tcp:servidor-de-hr-payroll-system.database.windows.net,1433;Initial Catalog=HR_PAYROLL_SYSTEM;Persist Security Info=False;User ID=Kevin;Password=Leandro123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            return new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase(connectionString);
         }
     }
 }
