f06c4173a300fcaef1e5261459c185f1c7adaf84
Kevin Leandro <leandrokevin576@gmail.com>
2021-01-29 10:54:58 -0600

    Envío de notificación de 10 días libres

--- Proyecto_HRPS/AdministradorInicioDeSesion.cs
+++ Proyecto_HRPS/AdministradorInicioDeSesion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,6 +20,7 @@ namespace Proyecto_HRPS
 
         private void botonDeIniciarSesion_Click(object sender, EventArgs e)
         {
+            generarCorreo();
             MenuDeAdministrador menuDeAdministrador = new MenuDeAdministrador();
             Empleado.Cedula = textBoxDeCedula.Text;
             this.Hide();
@@ -30,6 +32,61 @@ namespace Proyecto_HRPS
             MenuInicial menuInicial = new MenuInicial();
             this.Hide();
             menuInicial.Show();
+        }
+
+        public Database AbrirBaseDeDatos()
+        {
+            var connectionString = @"Server=tcp:servidor-de-hr-payroll-system.database.windows.net,1433;Initial Catalog=HR_PAYROLL_SYSTEM;Persist Security Info=False;User ID=Kevin;Password=Leandro123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            return new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase(connectionString);
+        }
+
+        private void generarCorreo()
+        {
+            var conexion = AbrirBaseDeDatos();
+            var comando = conexion.GetStoredProcCommand("MOSTRAR_EMPLEADOS_CON_DIAS_LIBRES");
+
+            using (IDataReader informacionEncontrada = conexion.ExecuteReader(comando))
+            {
+                if (informacionEncontrada.FieldCount != 0)
+                {
+                    if (informacionEncontrada.Read() == true)
+                    {
+                        //ENVIA UN CORREO
+                        AdministradorDeCorreo administradorDeCorreo = new AdministradorDeCorreo("smtp.gmail.com", "1037joseg@gmail.com", "Qwertz987.,!", 25);
+
+                        StringBuilder builder = new StringBuilder();
+
+                        builder.Append("<table class=table table-bordered align= center border= 1 cellpadding= 3 cellspacing= 0 width= 100%'>");
+                        builder.Append("<tr>");
+                        builder.Append("<th>CÉDULA</th>");
+                        builder.Append("<th>NOMBRE</th>");
+                        builder.Append("<th>CANTIDAD DE DÍAS DISPONIBLES</th>");
+                        builder.Append("</tr>");
+
+                        builder.Append("<tr align= center>");
+                        builder.Append("<td>" + informacionEncontrada.GetString(0) + "</td>");
+                        builder.Append("<td>" + informacionEncontrada.GetString(1) + "</td>");
+                        builder.Append("<td>" + informacionEncontrada.GetInt32(2).ToString() + "</td>");
+                        builder.Append("</tr>");
+                        builder.Append("</table>");
+
+                        var comando02 = conexion.GetStoredProcCommand("SACAR_CORREOS_DE_ADMINISTRADORES");
+                        string correoDeAdministrador = "";
+                        List<string> listaDeCorreos = new List<string>();
+                        listaDeCorreos.Add("leandrokevin576@gmail.com");
+                        using (IDataReader informacionEncontrada01 = conexion.ExecuteReader(comando02))
+                        {
+                            while (informacionEncontrada.Read())
+                            {
+                                correoDeAdministrador = informacionEncontrada01.GetString(0);
+                                listaDeCorreos.Add(correoDeAdministrador);
+                            }
+                        }
+                        administradorDeCorreo.EnviarCorreo("<h1>Los siguientes empleados </h1> <br/> " + builder.ToString(), "Notificación de 10 días libres", "1037joseg@gmail.com", "Electrónica UREBA S.A.", listaDeCorreos);
+                    }
+                }
+            }
+
         }
     }
 }
--- Proyecto_HRPS/SolicitudesVacaciones.cs
+++ Proyecto_HRPS/SolicitudesVacaciones.cs
@@ -38,5 +38,59 @@ namespace Proyecto_HRPS
             var connectionString = @"Server=tcp:servidor-de-hr-payroll-system.database.windows.net,1433;Initial Catalog=HR_PAYROLL_SYSTEM;Persist Security Info=False;User ID=Kevin;Password=Leandro123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             return new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase(connectionString);
         }
+        
+        private void enviarCorreoNotificacion()
+        {
+            var conexion = AbrirBaseDeDatos();
+            var comando = conexion.GetStoredProcCommand("EMPLEADO_INSERTAR_SOLICITUD_VACACIONES", textBoxDeNombre.Text,
+                                                                                                   dateTimePickerDeFechaDeInicio.Value,
+                                                                                                   dateTimePickerDeFechaDeFinalizacion.Value);
+            conexion.ExecuteNonQuery(comando);
+            //ENVIA UN CORREO
+            AdministradorDeCorreo administradorDeCorreo = new AdministradorDeCorreo("smtp.gmail.com", "1037joseg@gmail.com", "Qwertz987.,!", 587);
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append("<table class=table table-bordered align= center border= 1 cellpadding= 3 cellspacing= 0 width= 100%'>");
+            builder.Append("<tr>");
+            builder.Append("<th>NOMBRE</th>");
+            builder.Append("<th>FECHA DE INICIO</th>");
+            builder.Append("<th>FECHA DE FINALIZACIÓN</th>");
+            builder.Append("</tr>");
+
+            builder.Append("<tr align= center>");
+            builder.Append("<td>" + textBoxDeNombre.Text + "</td>");
+            builder.Append("<td>" + dateTimePickerDeFechaDeInicio.Value.ToString() + "</td>");
+            builder.Append("<td>" + dateTimePickerDeFechaDeFinalizacion.Value.ToString() + "</td>");
+            builder.Append("<td>" + "PENDIENTE" + "</td>");
+            builder.Append("</tr>");
+            builder.Append("</table>");
+
+
+            var comando02 = conexion.GetStoredProcCommand("[SACAR_CORREO_DE_EMPLEADO_CON_NOMBRE]", textBoxDeNombre.Text);
+            string correoDeEmpleado = "";
+            string correoDeAdministrador;
+            List<string> listaDeCorreos = new List<string>();
+            listaDeCorreos.Add("leandrokevin576@gmail.com");
+            using (IDataReader informacionEncontrada = conexion.ExecuteReader(comando02))
+            {
+                if (informacionEncontrada.Read())
+                {
+                    correoDeEmpleado = informacionEncontrada.GetString(0);
+                }
+            }
+            var comando03 = conexion.GetStoredProcCommand("[SACAR_CORREOS_DE_ADMINISTRADORES]");
+
+            using (IDataReader informacionEncontrada = conexion.ExecuteReader(comando03))
+            {
+                while (informacionEncontrada.Read())
+                {
+                    correoDeAdministrador = informacionEncontrada["CORREO"].ToString();
+                    listaDeCorreos.Add(correoDeAdministrador);
+                }
+            }
+            administradorDeCorreo.EnviarCorreo("<h1>Ha enviado una solicitud de vacaciones</h1> <br/> " + builder.ToString(), "Solicitud de vacaciones", "1037joseg@gmail.com", "Electrónica UREBA S.A.", new List<string> { correoDeEmpleado });
+            administradorDeCorreo.EnviarCorreo("<h1>Ha recibido una solicitud de vacaciones</h1> <br/> " + builder.ToString(), "Solicitud de vacaciones", "1037joseg@gmail.com", "Electrónica UREBA S.A.", listaDeCorreos);
+        }
     }
 }
